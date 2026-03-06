76cbf4d4d832971d1a5939c406ceafb328db4357
Caden <cadendouglas05@gmail.com>
2026-03-06 10:56:47 -0600

    worked on the car

---
+++ Scripts/car.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+var direction: String
+var speed: int = 500
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	match direction:
+		"NORTH":
+			position.y -= delta * speed
+func _car_setup(direction_in: String):
+	direction = direction_in
---
+++ Scripts/car_spawner.gd
@@ -0,0 +1,16 @@
+extends Area2D
+@export_enum("NORTH", "SOUTH", "EAST", "WEST") var direction: String = "NORTH"
+var car_scene = preload("res://Scenes/car.tscn")
+var chance: int = 500
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var randomroll = randi_range(0, chance)
+	if randomroll == 50:
+		var car = car_scene.instantiate()
+		car._car_setup(direction)
+		add_child(car)
