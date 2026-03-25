e87774f8efb5a1be5fe2839e3176198005ad9e59
Caden <cadendouglas05@gmail.com>
2026-03-25 15:56:44 -0500

    Added a tween for the story Scene

--- Scripts/story_screen.gd
+++ Scripts/story_screen.gd
@@ -14,7 +14,12 @@ func trigger_countdown():
 		$countdown_label.set_text(str(3 - i))
 		await get_tree().create_timer(1.0).timeout
 	$countdown_label.set_text("Click Space or Left Button To Continue")
+	fade_loop()
 	can_continue = true
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_accept") and can_continue:
 		get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
+func fade_loop():
+	var tween = create_tween().set_loops().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property($countdown_label, "modulate:a", 0.0, 1.0)
+	tween.tween_property($countdown_label, "modulate:a", 1.0, 1.0)
